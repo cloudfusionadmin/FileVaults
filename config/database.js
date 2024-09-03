0bbe90c994c7bba67b59bbf221026c098f8749ca
zevebe <support@cloudfusion.com.au>
2024-09-03 19:43:12 +1000

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,6 +3,7 @@ import { Sequelize } from 'sequelize';
 export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
+  dialectModule: pg,
   logging: false,
 });
 
