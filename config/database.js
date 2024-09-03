def03cd011e0ccb7656af0b32de27a8b275c3f6f
zevebe <support@cloudfusion.com.au>
2024-09-03 19:56:32 +1000

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,8 +3,11 @@ import pg from 'pg';
 
 export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'postgres',
   dialectModule: pg,
   logging: false,
+  dialectOptions: {
+    connectTimeout: 60000, // 60 seconds timeout for connecting
+  },
 });
-
